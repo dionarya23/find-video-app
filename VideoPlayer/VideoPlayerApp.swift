f4e21a35029bcbcd736810f1d5c9408893cbf838
Dion Arya Pamungkas <dionarya.p@gmail.com>
2023-09-15 22:00:59 +0700

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Dion Arya Pamungkas on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
