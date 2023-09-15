52eb0e76f815ad8c6dee577966d1b272788d0de0
Dion Arya Pamungkas <dionarya.p@gmail.com>
2023-09-15 23:59:37 +0700

    first commit

---
+++ VideoPlayer/VideoManager.swift
@@ -0,0 +1,84 @@
+//
+//  VideoManager.swift
+//  VideoPlayer
+//
+//  Created by Dion Arya Pamungkas on 15/09/23.
+//
+
+import Foundation
+
+
+enum Query: String, CaseIterable {
+    case nature, animals, people, ocean, food
+}
+
+class VideoManager: ObservableObject {
+    @Published private(set) var videos: [Video] = []
+    @Published var selectedQuery: Query = Query.nature {
+        didSet {
+            Task.init {
+                await findVideos(topic: selectedQuery)
+            }
+        }
+    }
+    
+    init() {
+        Task.init {
+            await findVideos(topic: selectedQuery)
+        }
+    }
+    
+    func findVideos(topic: Query) async {
+        do {
+            guard let url = URL(string: "https://api.pexels.com/videos/search?query=\(topic)&per_page=10&orientation=portrait") else { fatalError("MissingURL") }
+            
+            var urlRequest = URLRequest(url: url)
+            urlRequest.setValue("KkhlkI2UsOmM0mQlQzmTSwMS6kD81KDDbWrIRVT49eyUB2kBHdq72cBN", forHTTPHeaderField: "Authorization")
+            
+            let (data, response) = try await URLSession.shared.data(for: urlRequest)
+            
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("error while fetching data videos") }
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let decodedData = try decoder.decode(ResponseBody.self, from: data)
+
+            DispatchQueue.main.async {
+                self.videos = []
+                self.videos = decodedData.videos
+            }
+        } catch {
+            print("error while fetching data videos: \(error)")
+        }
+    }
+}
+
+struct ResponseBody: Decodable {
+    var page: Int
+    var perPage: Int
+    var totalResults: Int
+    var url: String
+    var videos: [Video]
+    
+}
+
+struct Video: Identifiable, Decodable {
+    var id: Int
+    var image: String
+    var duration: Int
+    var user: User
+    var videoFiles: [VideoFile]
+    
+    struct User: Identifiable, Decodable {
+        var id: Int
+        var name: String
+        var url: String
+    }
+    
+    struct VideoFile: Identifiable, Decodable {
+        var id: Int
+        var quality: String
+        var fileType: String
+        var link: String
+    }
+}
---
+++ VideoPlayer/Views/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  VideoPlayer
+//
+//  Created by Dion Arya Pamungkas on 15/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var videoManager = VideoManager()
+    var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+                    ForEach(Query.allCases, id: \.self) {
+                        searchQuery in
+                        QueryTag(query: searchQuery, isSelected: videoManager.selectedQuery == searchQuery)
+                            .onTapGesture {
+                                videoManager.selectedQuery = searchQuery
+                            }
+                    }
+                }
+                
+                ScrollView {
+                    if videoManager.videos.isEmpty {
+                        ProgressView()
+                    } else {
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(videoManager.videos, id: \.id) { video in
+                            NavigationLink {
+                                VideoView(video: video)
+                            } label: {
+                                VideoCard(video: video)
+                            }
+                          }
+                        }
+                        .padding()
+                    }
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .background(Color("AccentColor"))
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
